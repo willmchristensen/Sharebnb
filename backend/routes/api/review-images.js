2cb9f0c678a328609930bcbf68d2f68b5a83bd17
willmchristensen <johndoe@example.com>
2023-02-21 14:18:46 -0800

    owner authorization

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const { ReviewImage } = require('../../db/models');
+const { ReviewImage, Review} = require('../../db/models');
 const {handleValidationErrors} = require('../../utils/validation');
 const {requireAuth} = require('../../utils/auth');
 const router = express.Router();
-// TODO: implement owner scope
+// TODO: ?implement owner scope?
 // Delete a Review Image
 router.delete('/:reviewImageId',requireAuth,handleValidationErrors, async(req,res) => {
     const {reviewImageId} = req.params;
     const reviewImage = await ReviewImage.findByPk(reviewImageId);
-
+    const review = await Review.findByPk(reviewImage.reviewId);
+    let ownerId = review.userId;
     if(reviewImage){
-        await reviewImage.destroy()
-        res.status(200).json({message: "Successfully deleted"});
+        if(req.user.id === ownerId){
+            await reviewImage.destroy()
+            res.status(200).json({message: "Successfully deleted"});
+        }else{
+            res.status(400).json({message: "Review must belong to the current user"});
+        }
     }else{
         res.status(404).json({message: "ReviewImage couldn't be found"})
     }
