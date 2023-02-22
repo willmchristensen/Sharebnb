8f7de1270dd1091465c5b8fa3862d7b78ec48c04
willmchristensen <johndoe@example.com>
2023-02-22 10:52:04 -0800

    -

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class SpotImage extends Model {
     static associate(models) {
-      SpotImage.belongsTo(models.Spot, {foreignKey: 'spotId', onDelete: 'CASCADE'});
+      SpotImage.belongsTo(models.Spot, {foreignKey: 'spotId'});
     }
   }
   SpotImage.init({
