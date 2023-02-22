39dafed871de2515d7f4d6c43252c53da0e41595
willmchristensen <johndoe@example.com>
2023-02-22 11:05:22 -0800

    accidental onDelete cascade inside a belongsTo

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
     static associate(models) {
       Booking.belongsTo(models.User, {foreignKey: 'userId', onDelete: 'CASCADE'});
-      Booking.belongsTo(models.Spot, {foreignKey: 'spotId', onDelete: 'CASCADE'});
+      Booking.belongsTo(models.Spot, {foreignKey: 'spotId'});
     }
   }
   Booking.init({
