a525075fd8de75636abb3fd165ba79a42341b73f
willmchristensen <johndoe@example.com>
2023-02-22 10:37:38 -0800

    -

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -6,9 +6,9 @@ module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     static associate(models) {
       Spot.belongsTo(models.User, {foreignKey: 'ownerId'});
-      Spot.hasMany(models.SpotImage, {foreignKey: 'spotId'});
-      Spot.hasMany(models.Booking, {foreignKey: 'spotId'});
-      Spot.hasMany(models.Review, {foreignKey: 'spotId'});
+      Spot.hasMany(models.SpotImage, {foreignKey: 'spotId', onDelete: 'CASCADE'});
+      Spot.hasMany(models.Booking, {foreignKey: 'spotId', onDelete: 'CASCADE'});
+      Spot.hasMany(models.Review, {foreignKey: 'spotId', onDelete: 'CASCADE'});
     }
   }
   Spot.init({
