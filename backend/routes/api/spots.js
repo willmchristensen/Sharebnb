82d906c99d4c893df0897388d23b4dd7deea59fd
willmchristensen <johndoe@example.com>
2023-02-26 22:54:39 -0800

    shtuff

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -27,17 +27,22 @@ const validateSpot = [
       .exists({ checkFalsy: true })
       .notEmpty()
       .withMessage('Country is required'),
-    check('lat')
+      check('lat')
       .exists({ checkFalsy: true })
       .notEmpty()
       .withMessage('Latitude is required'),
+    check('lat')
+      .isFloat({min:-90, max: 90})
+      .withMessage('Latitude is not valid'),
     check('lng')
       .exists({ checkFalsy: true })
       .notEmpty()
       .withMessage('Longitude is required'),
+    check('lng')
+      .isFloat({min:-180, max: 180})
+      .withMessage('Longitude is not valid'),
     check('name')
-      .exists({ checkFalsy: true })
-      .notEmpty()
+      .isLength({max: 50})
       .withMessage('Name must be less than 50 characters'),
     check('description')
       .exists({ checkFalsy: true })
@@ -241,6 +246,7 @@ router.put('/:spotId',requireAuth,validateSpot, async(req,res) => {
             spot.price =price ;
 
             await spot.save();
+
 
             const revisedSpot =  await Spot.findOne({
                 where: {
