3267d0fec409eb94525c5326405fccc1ab488ca0
willmchristensen <johndoe@example.com>
2023-03-12 22:50:36 -0700

    post a review modal work--- lots left!

--- frontend/src/components/PostAReviewModal/stars.js
+++ frontend/src/components/PostAReviewModal/stars.js
@@ -1,67 +1,58 @@
 import {useEffect, useState} from 'react';
 import './stars.css'
 
-function StarsRatingInput(rating) {
+function StarsRatingInput() {
 
  // const handleClick = (newRating) => {
  //    onChange(newRating);
  //  }
   // TODO: ACTIVERATING
-  const [activeRating,setActiveRating] = useState({rating});
-  const starClass = activeRating >= 1 ? 'filled-star' : 'empty-star'
+  // const [activeRating,setActiveRating] = useState({rating});
+  const [activeRating,setActiveRating] = useState(0);
+  let rating = 5;
+  let empty = <i className="far fa-star"></i>;
+  let filled = <i className="fas fa-star"></i>;
  //  // useEffect(() => {
  //  //   setActiveRating()
  //  // }, [activeRating])
   return (
-   <div className="rating-input">
-       <div 
-        className='filled-star'
-        //  className={activeRating >= 1 ? 'filled' : 'empty'} 
-        //  onClick={()=>handleClick()}
-       >
-        <i class="fal fa-stars"></i>
-       </div>
-       <div 
-        //  className={activeRating >= 2 ? 'filled' : 'empty'}
-        //  onClick={()=>onChange({activeRating})}
-       >
-         <i 
-           className="fa-regular fa-star"
-          //  onMouseEnter={()=>setActiveRating(2)}
-          //  onMouseLeave={() => setActiveRating(rating)}
-         ></i>
-       </div>
-       <div 
-        //  className={activeRating >= 3 ? 'filled' : 'empty'}
-        //  onClick={()=>onChange({activeRating})}
-       >
-         <i 
-           className="fa-regular fa-star"
-          //  onMouseEnter={()=>setActiveRating(3)}
-          //  onMouseLeave={() => setActiveRating(rating)}
-         ></i>
-       </div>
-       <div 
-        //  className={activeRating >= 4 ? 'filled' : 'empty'}
-        //  onClick={()=>onChange({activeRating})}
-       >
-         <i 
-           className="fa-regular fa-star"
-          //  onMouseEnter={()=>setActiveRating(4)}
-          //  onMouseLeave={() => setActiveRating(rating)}
-         ></i>
-       </div>
-       <div 
-        //  className={activeRating >= 5 ? 'filled' : 'empty'}
-        //  onClick={()=>onChange({activeRating})}
-       >
-         <i 
-           className="fa-regular fa-star"
-          //  onMouseEnter={()=>setActiveRating(5)}
-          //  onMouseLeave={() => setActiveRating(rating)}
-         ></i>
-       </div>
-   </div>
+    <div className="stars">
+      <div 
+        className="star"   
+        onMouseEnter={() => setActiveRating(1)}
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {activeRating >= 1 ? filled : empty}
+      </div>
+      <div 
+        className="star"   
+        onMouseEnter={() => setActiveRating(2)}
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {activeRating >= 2 ? filled : empty}
+      </div>
+      <div 
+        className="star"   
+        onMouseEnter={() => setActiveRating(3)}
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {activeRating >= 3 ? filled : empty}
+      </div>
+      <div 
+        className="star"   
+        onMouseEnter={() => setActiveRating(4)}
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {activeRating >= 4 ? filled : empty}
+      </div>
+      <div 
+        className="star"   
+        onMouseEnter={() => setActiveRating(5)}
+        onMouseLeave={() => setActiveRating(rating)}
+      >
+        {activeRating >= 5 ? filled : empty}
+      </div>
+    </div>
   ); 
 };
 
