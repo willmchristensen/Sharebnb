ae411d00cfea1a026ab394a3c7f47db7c85c5d22
willmchristensen <johndoe@example.com>
2023-03-15 04:53:40 -0700

    manage spots bug?

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -18,38 +18,42 @@ const ManageSpots = () => {
   useEffect(() => {
     dispatch(loadUserSpots())
   }, [dispatch]) 
-  let spots = useSelector(state => state.spots.allSpots);
-  // // console.log('----------------spotsspotsspotsspotsspotsspotsspotsspots',spots);
-  let allSpots = Object.values(spots);
-  // console.log('------------------------------allspots', allSpots);
+  let spots = Object.values(useSelector(state => state.spots.allSpots));
+  console.log('----------------spotsspotsspotsspotsspotsspotsspotsspots',spots);
+  let user = useSelector(state => state.session.user);
+  console.log('------------------------------user', user);
+  let allSpots = spots.find(spot => spot.ownerId === user.id);
+  console.log('------------------------------allSpots', typeof allSpots);
+  let allSpotsArray = Object.values(allSpots)
+  console.log('------------------------------ARRAY', typeof allSpots);
 
-  return (
-    <main >
-      <div className="manage-wrapper">
-        <nav className="spot-cards-section">
-          <div className="spot-cards-section-header">
-            <h3>Manage Your Spots</h3>
-            <NavLink
-              className="spot-card"
-              to={`/spots/new`}
-            >
-              <button id="manage-button">create a new spot</button>
-            </NavLink>
-          </div>
-          {
-            allSpots.map(spot => {
-              return (
-                <>
-                  <SpotCards spot={spot}></SpotCards>
-                  <ManageButtons spot={spot}></ManageButtons>
-                </>
-              )
-            })
-          }
-        </nav>
-      </div>
-    </main>
-  );
+  // return (
+  //   <main >
+  //     <div className="manage-wrapper">
+  //       <nav className="spot-cards-section">
+  //         <div className="spot-cards-section-header">
+  //           <h3>Manage Your Spots</h3>
+  //           <NavLink
+  //             className="spot-card"
+  //             to={`/spots/new`}
+  //           >
+  //             <button id="manage-button">create a new spot</button>
+  //           </NavLink>
+  //         </div>
+  //         {
+  //           allSpotsArray.map(spot => {
+  //             return (
+  //               <>
+  //                 <SpotCards spot={spot}></SpotCards>
+  //                 <ManageButtons spot={spot}></ManageButtons>
+  //               </>
+  //             )
+  //           })
+  //         }
+  //       </nav>
+  //     </div>
+  //   </main>
+  // );
 };
 
 export default ManageSpots;
