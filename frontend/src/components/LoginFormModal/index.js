b4ec7dffe4abadd3028543b388dc31105d2ea275
willmchristensen <johndoe@example.com>
2023-04-07 20:56:02 -0700

    login

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -8,32 +8,35 @@ function LoginFormModal() {
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
-  const [isDisabled,setIsDisabled] = useState(true);
+  // const [isDisabled,setIsDisabled] = useState(true);
   const { closeModal } = useModal();
-  useEffect(() => {
-    if(
-        credential.length < 4 ||
-        password.length < 6
-      ){
-        setIsDisabled(true)
-      }else{
-        setIsDisabled(false)
-      }
-  },[credential,password])
+  // useEffect(() => {
+  //   if(
+  //       credential.length < 4 ||
+  //       password.length < 6
+  //     ){
+  //       setIsDisabled(true)
+  //     }else{
+  //       setIsDisabled(false)
+  //     }
+  // },[credential,password])
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
-    const errors = {};
-    if(password.length < 6) errors.password = 'Password must be longer than 6 characters.';
-    if(credential.length < 4) errors.credential = 'Credential must be longer than 4 characters.';
-    setErrors(errors);
+    // const errors = {};
+    // if(password.length < 6) errors.password = 'Password must be longer than 6 characters.';
+    // if(credential.length < 4) errors.credential = 'Credential must be longer than 4 characters.';
+    // setErrors(errors);
     return dispatch(sessionActions.login({ credential, password }))
       .then(closeModal)
       .catch(
         async (res) => {
           const data = await res.json();
-          if (data){
-            setErrors("The provided credentials were invalid");
+          if (data && data.message){
+            setErrors(data.message);
+            // setErrors(data.message);
+            console.log(data);
+            console.log('ERRORS:',errors);
           } 
         }
       );
@@ -45,11 +48,12 @@ function LoginFormModal() {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <ul>
-          {<div className="errors">{Object.values(errors)}</div>}
-        </ul>
         <div className="user-information">
           <h1>Log In</h1>
+          <p className="errors">{errors}</p>
+          {/* <div>
+            {errors && <p className="errors">{errors}</p>}
+          </div> */}
           <label>
             <input
               type="text"
@@ -68,12 +72,22 @@ function LoginFormModal() {
               placeholder="Password"
             />
           </label>
-        <div className="login-buttons">
-          <button type="submit" disabled={isDisabled} id="button">Log In</button>
-          <button type="submit" onClick={handleDemoUser} id="button">
-            Demo User
-          </button>
-        </div>
+          <div className="login-buttons">
+            <button 
+              type="submit" 
+              id="button" 
+              onClick={handleSubmit}
+            >
+              Log In
+            </button>
+            <button 
+              type="submit" 
+              onClick={handleDemoUser} 
+              id="button"
+            >
+              Demo User
+            </button>
+          </div>
         </div>
       </form>
     </>
