99432557d39ce1f8ed3f4163e8c11b090fb493d9
willmchristensen <willmchristensen@gmail.com>
2023-04-11 22:41:36 -0700

    mobile friendly menu - needs some work and need to clean up transitions between difft sizes.

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,54 +8,107 @@ function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul className="nav-bar">
-      <li
-        id="logo"
-        className="nav-bar-conditional-content"
-      >
-        <NavLink exact to="/">
-          {/* <div className="nav-item"> */}
-            <i class="fas fa-yin-yang"></i>
-          {/* </div> */}
-          <span className="logo">
-            ShareBnB
-          </span>
-        </NavLink>
-      </li>
-      <div
-        className="nav-bar-conditional-content"
-        id="profile-bruh"
-      >
-        {
-          sessionUser && (
+    <>
+      <ul className="nav-bar">
+        <li
+          id="logo"
+          className="nav-bar-conditional-content"
+        >
+          <NavLink exact to="/">
+            {/* <div className="nav-item"> */}
+              <i class="fas fa-yin-yang"></i>
+            {/* </div> */}
+            <span className="logo">
+              ShareBnB
+            </span>
+          </NavLink>
+        </li>
+        <div
+          className="nav-bar-conditional-content"
+          id="profile-bruh"
+        >
+          {
+            sessionUser && (
+              <>
+                <li
+                  id="new-spot"
+                >
+                  <NavLink
+                    to={`/spots/new`}
+                  >
+                    Create A New Spot
+                  </NavLink>
+                </li>
+              </>
+            )
+          }
+          {
+            isLoaded && (
             <>
               <li
-                id="new-spot"
+                id="profile"
               >
-                <NavLink
-                  to={`/spots/new`}
-                >
-                  Create A New Spot
-                </NavLink>
+                <div className="nav-item">
+                  <ProfileButton user={sessionUser} />
+                </div>
               </li>
             </>
-          )
-        }
-        {
-          isLoaded && (
-          <>
-            <li
-              id="profile"
-            >
-              <div className="nav-item">
-                <ProfileButton user={sessionUser} />
-              </div>
-            </li>
-          </>
-          )
-        }
+            )
+          }
+        </div>
+      </ul>
+      <div className="mobile-nav">
+        <ul className="baby-menu">
+          <li
+            id="logo"
+            className="nav-bar-conditional-content"
+          >
+            <NavLink exact to="/">
+              {/* <div className="nav-item"> */}
+                <i class="fas fa-yin-yang"></i>
+              {/* </div> */}
+              <span className="logo">
+                ShareBnB
+              </span>
+            </NavLink>
+          </li>
+          <div
+            className="nav-bar-conditional-content"
+            id="profile-bruh"
+          >
+            {
+              sessionUser && (
+                <>
+                  <li
+                    id="new-spot"
+                  >
+                    <NavLink
+                      to={`/spots/new`}
+                    >
+                      Create A New Spot
+                    </NavLink>
+                  </li>
+                </>
+              )
+            }
+            {
+              isLoaded && (
+              <>
+                <li
+                  id="profile"
+                >
+                  <div className="nav-item">
+                    <ProfileButton user={sessionUser} />
+                  </div>
+                </li>
+              </>
+              )
+            }
+          </div>
+        </ul>
       </div>
-    </ul>
+    </>
+
   );
 }
 
