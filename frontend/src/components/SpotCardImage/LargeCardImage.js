ab1ce91af8d4fb3aeebfabd92dd14eda7b31c063
willmchristensen <willmchristensen@gmail.com>
2023-11-02 13:10:00 -0700

    default large spot image

--- frontend/src/components/SpotCardImage/LargeCardImage.js
+++ frontend/src/components/SpotCardImage/LargeCardImage.js
@@ -1,9 +1,13 @@
 import './LargeCardImage.css'
 
 const LargeCardImage = ({image}) => {
+   const defaultImg = "https://static.thenounproject.com/png/4974686-200.png";
+    const handleImageError = (event) => {
+        event.target.src = defaultImg;
+    }
   return (
     <div className="large-spot-card-image">
-        <img src={image} alt="hero-image" />
+        <img src={image} alt="hero-image" onError={handleImageError}/>
     </div>
   );
 };
